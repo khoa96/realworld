3ddf7c2c2eea2a5f6eb1809a5d2258662eb0b3b4
khoa96 <khoanguyenptit96@gmail.com>
2019-05-23 10:04:07 +0700

    commit 2

--- src/components/Home/test.js
+++ src/components/Home/test.js
@@ -4,7 +4,7 @@ export default class test extends Component {
     render() {
         return (
             <div>
-                Nguyen dang khoa
+                Nguyen dang khoa hoc vien cong nghe buu chinh vien thong
             </div>
         )
     }
